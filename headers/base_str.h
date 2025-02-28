61c75fbe53cce63e4a6ea1472e5c6d43b916b8e1
Joshua Matthews <jmatth93@gmail.com>
2025-02-28 15:20:24 -0600

    adding strings

---
+++ headers/base_str.h
@@ -0,0 +1,98 @@
+#ifndef CSTD_BASE_C_STR
+#define CSTD_BASE_C_STR
+
+#include <stdlib.h>
+
+/**
+ * Enums for error base_str error codes.
+ */
+typedef enum base_str_error {
+    /* success */
+    C_STR_NO_ERROR,
+    /* malloc error */
+    C_STR_MALLOC_ERROR,
+    /* out of bounds */
+    C_STR_OUT_OF_BOUNDS
+} base_str_error;
+
+/**
+ * Internal base_str structure.
+ */
+struct internal_base_str;
+
+/**
+ * Structure to handle dealing with strings in a more convenient way.
+ */
+typedef struct base_str_t {
+    /* internal */
+    struct internal_base_str *_internal;
+    /**
+     * Append the given string to this string.
+     * @param[in] struct base_str* The base_str object.
+     * @param[in] const char* The string to append.
+     * @param[in] size_t The size of the string to copy.
+     * @returns base_str_error.
+     */
+    base_str_error (*append)(struct base_str_t*, const char *, size_t);
+    /**
+     * Get the length of the string. (does not include null-terminator
+     * @param[in] const struct base_str* The base_str structure.
+     * @returns The length.
+     */
+    int (*length)(const struct base_str_t*);
+    /**
+     * Get the underlying string.
+     * @param[in] const struct base_str* The base_str structure.
+     * @param[out] char** The pointer to return out the string to.
+     * @returns base_str_error.
+     */
+    base_str_error (*get_str)(const struct base_str_t*, char **);
+    /**
+     * Get the underlying string as a const char*.
+     * @param[in] const struct base_str* The base_str structure.
+     * @param[out] const char** The pointer to return out the string to.
+     * @returns base_str_error.
+     */
+    base_str_error (*get_const_str)(const struct base_str_t*, const char **);
+    /**
+     * Set the underlying string with the given string.
+     * @param[in] struct base_str* The base_str structure.
+     * @param[in] const char* The string data.
+     * @param[in] size_t The size of the given string.
+     * @returns base_str_error.
+     */
+    base_str_error (*set)(struct base_str_t*, const char *, const size_t);
+    /**
+     * Get the character at the given index.
+     * @param[in] struct base_str* The base_str structure.
+     * @param[in] size_t The index value.
+     * @param[out] char* The out param.
+     * @returns base_str_error.
+     */
+    base_str_error (*at)(const struct base_str_t*, const size_t, char*);
+} base_str;
+
+/**
+ * Initializes a new base_str structure with a certain size.
+ * @param[in] struct base_str* The base_str structure to initialize.
+ * @param[in] size_t The size to initialize to.
+ * @returns base_str_error.
+ */
+base_str_error new_base_str(struct base_str_t*, size_t);
+
+/**
+ * Initializes a new base_str structure with a given string.
+ * @param[in] struct base_str* The base_str structure to initialize.
+ * @param[in] const char* The string to copy.
+ * @param[in] size_t The size of the given string.
+ * @returns base_str_error.
+ */
+base_str_error new_base_str_with_string(struct base_str_t*, const char*, size_t);
+
+/**
+ * Frees the internals of the base_str.
+ * @param[in] struct base_str* The base_str to free.
+ */
+void free_base_str(struct base_str_t*);
+
+#endif
--- headers/unicode_str.h
+++ headers/unicode_str.h
@@ -1,27 +1,51 @@
 #ifndef CSTD_UNICODE_STR_H
 #define CSTD_UNICODE_STR_H
 
+#include "defs.h"
 #include "utf8.h"
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "array_template.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+__BEGIN_DECLS
 
 typedef uint32_t code_point_t;
 
 generate_array_template(char, char);
 
-struct unicode_t {
-  char_array str;
-};
+struct unicode_str_t;
 
+struct unicode_str_t *unicode_str_create() __THROWNL;
 
+bool unicode_str_set(struct unicode_str_t *str, const char *other,
+                     size_t len) __THROWNL __nonnull((1, 2));
 
-#ifdef __cplusplus
-}
-#endif
+bool unicode_str_get(struct unicode_str_t *str, const char_array *out) __THROWNL
+    __nonnull((1));
+
+bool unicode_str_append(struct unicode_str_t *str, const char *other,
+                        size_t len) __THROWNL __nonnull((1, 2));
+
+bool unicode_str_insert_at(struct unicode_str_t *str, const char *other,
+                           size_t len, size_t offset) __THROWNL
+    __nonnull((1, 2));
+
+bool unicode_str_remove_range(struct unicode_str_t *str, size_t offset,
+                              size_t len) __THROWNL __nonnull((1));
+
+size_t unicode_str_len(struct unicode_str_t *str) __THROWNL __nonnull((1));
+
+size_t unicode_str_byte_len(struct unicode_str_t *str) __THROWNL __nonnull((1));
+
+bool unicode_str_codepoint_at(struct unicode_str_t *str, size_t index,
+                              code_point_t *out) __THROWNL __nonnull((1));
+
+bool unicode_str_get_range(struct unicode_str_t *str, size_t index, size_t len,
+                           const char **out) __THROWNL __nonnull((1));
+
+void unicode_str_destroy(struct unicode_str_t *str) __THROWNL __nonnull((1));
+
+__END_DECLS
 
 #endif
