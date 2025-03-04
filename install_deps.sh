0aa68206a1d4b548157a6077bbfcd83747cf63d3
Joshua Matthews <jmatth93@gmail.com>
2025-03-04 11:21:22 -0600

    include zig install

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt install -y snapd
+sudo snap install zig --classic --beta
 
 if [ ! -d ./deps/utf8-zig ]; then
   git clone https://github.com/jmatth11/utf8-zig.git deps/utf8-zig
