eb8c026469c80fe101beb2b3a62c4ffddb4fa930
Joshua Matthews <jmatth93@gmail.com>
2025-03-10 15:13:04 -0500

    fix script to only download from apt-get if needed

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-sudo apt install -y snapd
-sudo snap install zig --classic --beta
+which zig > /dev/null
+if [ $? -ne 0 ]; then
+  sudo apt install -y snapd
+  sudo snap install zig --classic --beta
+fi
 
 if [ ! -d ./deps/utf8-zig ]; then
   git clone https://github.com/jmatth11/utf8-zig.git deps/utf8-zig
