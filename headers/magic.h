cd84fbe6fe63e7057febd0cee4755a1da892498c
Joshua Matthews <jmatth93@gmail.com>
2025-04-12 11:20:34 -0500

    fixed reference to functions

--- headers/magic.h
+++ headers/magic.h
@@ -1,8 +1,6 @@
 #ifndef CSTD_MAGIC_H
 #define CSTD_MAGIC_H
 
-#include <stdlib.h>
-
 #define DEFER(func) __attribute__((__cleanup__(func)))
 
 inline void cstd_freep_fn(void**);
@@ -11,10 +9,10 @@ inline void cstd_freei_fn(int**);
 inline void cstd_freed_fn(double**);
 inline void cstd_freef_fn(float**);
 
-#define AUTO_P DEFER(__cstd_freep_fn)
-#define AUTO_C DEFER(__cstd_freec_fn)
-#define AUTO_I DEFER(__cstd_freei_fn)
-#define AUTO_D DEFER(__cstd_freed_fn)
-#define AUTO_F DEFER(__cstd_freef_fn)
+#define AUTO_P DEFER(cstd_freep_fn)
+#define AUTO_C DEFER(cstd_freec_fn)
+#define AUTO_I DEFER(cstd_freei_fn)
+#define AUTO_D DEFER(cstd_freed_fn)
+#define AUTO_F DEFER(cstd_freef_fn)
 
 #endif
