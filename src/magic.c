bf1a32093729054573566f6d7d9b44a3358f2684
Joshua Matthews <jmatth93@gmail.com>
2025-04-14 11:23:33 -0500

    fixed warnings

--- src/magic.c
+++ src/magic.c
@@ -2,24 +2,16 @@
 
 #include <stdlib.h>
 
-inline void cstd_freep_fn(void **p) {
-  if (*p == NULL) return;
+void cstd_freep_fn(void **p) {
+  if (*p == NULL)
+    return;
   free(*p);
   *p = NULL;
 }
 
-inline void cstd_freec_fn(char**c) {
-  cstd_freep_fn((void**)c);
-}
-inline void cstd_freei_fn(int**i) {
-  cstd_freep_fn((void**)i);
-}
+void cstd_freec_fn(char **c) { cstd_freep_fn((void **)c); }
+void cstd_freei_fn(int **i) { cstd_freep_fn((void **)i); }
 
-inline void cstd_freed_fn(double**d) {
-  cstd_freep_fn((void**)d);
-}
+void cstd_freed_fn(double **d) { cstd_freep_fn((void **)d); }
 
-inline void cstd_freef_fn(float**f) {
-  cstd_freep_fn((void**)f);
-}
-
+void cstd_freef_fn(float **f) { cstd_freep_fn((void **)f); }
