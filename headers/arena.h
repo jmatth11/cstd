643a4a4dbcf4f5abd83de898ab09fcfa206eff96
Joshua Matthews <jmatth93@gmail.com>
2025-07-03 13:35:52 -0500

    added simple arena allocator

---
+++ headers/arena.h
@@ -0,0 +1,38 @@
+#ifndef CSTD_ARENA_ALLOCATOR_H
+#define CSTD_ARENA_ALLOCATOR_H
+
+#include <stddef.h>
+
+/**
+ * Simple arena allocator
+ */
+struct arena_t;
+
+/**
+ * Create an arena allocator.
+ *
+ * @param size The size of the arena.
+ * @return Newly allocated arena.
+ */
+struct arena_t* arena_create(size_t size);
+
+/**
+ * Allocate a new object from the arena allocator.
+ *
+ * @param ptr The arena.
+ * @param size The size of the allocation.
+ * @param alignment The alignment of the type. @see _Alignof
+ * @return The newly allocated object, NULL if an error occurred.
+ */
+void* arena_alloc(struct arena_t *ptr, size_t size, size_t alignment);
+
+/**
+ * Free the arena allocator.
+ *
+ * @param ptr The arena.
+ */
+void arena_destroy(struct arena_t** ptr);
+
+
+
+#endif
