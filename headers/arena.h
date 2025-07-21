603bcbea497e8833778053cad4ca2030f2d8f621
Joshua Matthews <jmatth93@gmail.com>
2025-07-21 13:27:47 -0500

    added support for growing allocator

--- headers/arena.h
+++ headers/arena.h
@@ -11,14 +11,20 @@ struct arena_t;
 
 /**
  * Create an arena allocator.
+ * The arena will allocate blocks of memory in page_size chunks.
+ * Once the block of memory is completely claimed another block will be allocated.
  *
- * @param size The size of the arena.
+ * @param size The page size of the arena.
  * @return Newly allocated arena.
  */
-struct arena_t* arena_create(size_t size);
+struct arena_t* arena_create(size_t page_size);
 
 /**
  * Allocate a new object from the arena allocator.
+ * This allocates an object on a free section of the memory block.
+ * If there is no free space, a new block is allocated.
+ * If the size of the object is larger than the page_size this function
+ *  fails and returns NULL.
  *
  * @param ptr The arena.
  * @param size The size of the allocation.
--- src/arena.c
+++ src/arena.c
@@ -7,16 +7,18 @@ struct arena_t {
   size_t size;
   size_t offset;
   void *ptr;
+  struct arena_t *next;
 };
 
-struct arena_t* arena_create(size_t size) {
+struct arena_t* arena_create(size_t page_size) {
   struct arena_t *result = malloc(sizeof(struct arena_t));
   if (result == NULL) {
     return NULL;
   }
-  result->size = size;
+  result->size = page_size;
   result->offset = 0;
-  result->ptr = malloc(size);
+  result->ptr = malloc(page_size);
+  result->next = NULL;
   if (result->ptr == NULL) {
     free(result);
     return NULL;
@@ -24,14 +26,28 @@ struct arena_t* arena_create(size_t size) {
   return result;
 }
 
+static void* arena_get_next_alloc(struct arena_t *ptr, size_t size, size_t alignment) {
+  if (ptr->next == NULL) {
+    ptr->next = arena_create(ptr->size);
+    return arena_alloc(ptr->next, size, alignment);
+  }
+  return arena_alloc(ptr->next, size, alignment);
+}
+
 void* arena_alloc(struct arena_t *ptr, size_t size, size_t alignment) {
+  // size is bigger than arena page size
+  if (size > ptr->size) {
+    return NULL;
+  }
+  if (ptr->offset == ptr->size) {
+    return arena_get_next_alloc(ptr, size, alignment);
+  }
   // get alignment padding to conform to CPU memory layout
   size_t padding = (alignment - (ptr->offset % alignment)) % alignment;
   size_t aligned_offset = ptr->offset + padding;
   size_t new_offset = aligned_offset + size;
-  if (size > ptr->size) {
-    // TODO figure out how to increase size without invalidating previous pointer
-    return NULL;
+  if (new_offset > size) {
+    return arena_get_next_alloc(ptr, size, alignment);
   }
   void *result = (void*)(ptr->ptr + aligned_offset);
   ptr->offset = new_offset;
@@ -48,6 +64,9 @@ void arena_destroy(struct arena_t** ptr) {
   }
   (*ptr)->size = 0;
   (*ptr)->offset = 0;
+  if ((*ptr)->next != NULL) {
+    arena_destroy( &((*ptr)->next) );
+  }
   free((*ptr)->ptr);
   free(*ptr);
   *ptr = NULL;
